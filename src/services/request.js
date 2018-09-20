bbca53b45e6cc5b240901121933545904f9994f5
annaelde <elde.av@gmail.com>
2018-09-20 19:24:03 -0400

    rework request service.

--- src/services/request.js
+++ src/services/request.js
@@ -4,29 +4,42 @@ const headers = new Headers({
 })
 
 const request = {
-    get: async (url, payload = {}) => {
-        let request =  new Request(`${baseURL}${url}`, {
-            method: 'GET',
-            headers
-        })
-        return fetch(request)
-    },
+    get: async (url, payload = {}) =>
+        await fetch(
+            new Request(`${baseURL}${url}`, {
+                method: 'GET',
+                headers
+            })
+        ).then(response => parseJSON(response)),
     post: async (url, payload = {}) =>
-        fetch(
+        await fetch(
             new Request(`${baseURL}${url}`, {
                 method: 'POST',
                 headers,
                 body: JSON.stringify(payload)
             })
-        ),
+        ).then(response => parseJSON(response)),
     delete: async (url, payload = {}) =>
-        fetch(
+        await fetch(
             new Request(`${baseURL}${url}`, {
                 method: 'DELETE',
                 headers,
                 body: JSON.stringify(payload)
             })
-        )
+        ).then(response => parseJSON(response))
+}
+
+function parseJSON(response) {
+    return new Promise((resolve, reject) => {
+        return response.text().then(text => {
+            let json = text.length ? JSON.parse(text) : null
+            if (response.ok) {
+                resolve(json)
+            } else {
+                reject(json)
+            }
+        })
+    })
 }
 
 function setToken(token) {
